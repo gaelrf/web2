05f119b937faa8b394a51314e9991eee5b48cf4e
gaelrf <gaelrey99@gmail.com>
2024-12-27 17:30:14 +0100

    login

---
+++ js/login.js
@@ -0,0 +1,13 @@
+var login = document.getElementById("formulario")
+
+login.onsubmit = function (e){
+    e.preventDefault()
+    var nombre = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("password").value;
+    if (nombre == "Administrador" && contraseña == "1234") {
+        window.location.href = "main.html"
+    } else {
+        alert("Usuario o contraseña incorrectos")
+    }
+
+}

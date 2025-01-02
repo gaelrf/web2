318f972a9ef46d63935955e0cb6e97c0a1eadb35
gaelrf <gaelrey99@gmail.com>
2025-01-02 19:56:13 +0100

    Main works

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var imgusuario = document.getElementById('imguser')
+var menu = document.getElementById('menumovil')
+
+imgusuario.onclick = dropdownmenu
+menu.onclick = menumovil
+
+function dropdownmenu() {
+    var dropdownmenu = document.getElementById('menu')
+    if (dropdownmenu.style.display == 'block') {
+        dropdownmenu.style.display = 'none'
+    } else {
+        dropdownmenu.style.display = 'block'
+    }
+}
+
+function menumovil() {
+    var menuoculto = document.getElementById('menuoculto')
+    if (menuoculto.style.display == 'block') {
+        menuoculto.style.display = 'none'
+    } else {
+        menuoculto.style.display = 'block'
+
+    }
+}
